6bf9b473a98d87d244eaf626f05db0a16006034c
Kayd Withers <kaydwithers@gmail.com>
2018-10-22 23:38:34 +1100

    commit

--- src/store.js
+++ src/store.js
@@ -1,10 +1,31 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
-});
+  state: {
+    user: {
+      id: 'abc123',
+      name: 'Kayd Withers'
+    },
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing']
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event)
+      commit('ADD_EVENT', event)
+    }
+  },
+  getters: {
+    getEventById: state => id => {
+      return state.categories.find(event => event.id === id)
+    }
+  }
+})
